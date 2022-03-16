5337949ab725bd112ad667e929a59419b0640d11
Jinh <eful5678@gmail.com>
2022-03-16 19:18:24 +0900

    [수정] router 및 axios 사용 예시

--- src/components/BoardHeader.vue
+++ src/components/BoardHeader.vue
@@ -1,29 +1,62 @@
 <template>
-  <header> 
-    <h1><router-link to="/main"><img alt="boardlogo" src="../assets/boardlogo.png" width="90"></router-link></h1>
+  <header>
+    <h1>
+      <router-link to="/main"
+        ><img alt="boardlogo" src="../assets/boardlogo.png" width="90"
+      /></router-link>
+    </h1>
+    <!-- <h1>
+      <a @click="toMain">메인으루</a>
+    </h1> -->
     <div class="menuWrap">
-              <ul class="menu">
-                <li><router-link to="/board/list">Main Board</router-link></li>
-				
-              </ul>
-
+      <ul class="menu">
+        <li><router-link to="/board/list">Main Board</router-link></li>
+      </ul>
     </div>
-  
-    </header>
+  </header>
 </template>
 
 <script>
 export default {
-
-}
+  methods: {
+    // toMain: function () {
+    //   this.$router.push("/main/list");
+    // },
+  },
+};
 </script>
 
 <style scoped>
-    header{width:100%; text-align:center; position:relative; height:120px; border-bottom:1px solid #35495e}
-    header h1{position:absolute; top:0; left:100px;}
-    header ul.menu:after{display:block; clear:both; content:'';}
-    header ul.menu{position:absolute; top:20px; right:50px;}
-    header ul.menu li{float:left; padding:10px 20px; list-style:none;}
+header {
+  width: 100%;
+  text-align: center;
+  position: relative;
+  height: 120px;
+  border-bottom: 1px solid #35495e;
+}
+header h1 {
+  position: absolute;
+  top: 0;
+  left: 100px;
+}
+header ul.menu:after {
+  display: block;
+  clear: both;
+  content: "";
+}
+header ul.menu {
+  position: absolute;
+  top: 20px;
+  right: 50px;
+}
+header ul.menu li {
+  float: left;
+  padding: 10px 20px;
+  list-style: none;
+}
 
-a{text-decoration:none; color:#333;}
+a {
+  text-decoration: none;
+  color: #333;
+}
 </style>
