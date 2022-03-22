38ddbcd56db5297aef938101e3be0f2af962ba30
Jinh <eful5678@gmail.com>
2022-03-23 08:15:52 +0900

    [수정] board 리스트 조회 함수 실행시점 주석

--- src/components/BoardList.vue
+++ src/components/BoardList.vue
@@ -43,6 +43,12 @@ export default {
       content: "",
     },
   }),
+  // BoardList 페이지에 들어오면 따로 버튼이 아니라 바로 board가 조회될 수 있도록
+  // created 단계에 조회 함수를 실행
+  // 일반적으로 조회함수 이름을 search라 하는편, 순수한 검색은 다른이름으로 다시만들것
+  created() {
+    // this.search();
+  },
   methods: {
     write: function () {
       this.$router.push("/write");
