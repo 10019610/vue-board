b50574e54666b481581ab282a05008ecf45000de
young jo <a10019610@gmail.com>
2022-03-25 00:32:49 +0900

    [수정] 리스트 구현
    화면에 띄움

--- src/components/BoardList.vue
+++ src/components/BoardList.vue
@@ -7,20 +7,15 @@
         value="검색"
         @click="search"/>
         
-    </div> -->                    
-    <div>
-      <li v-for="(title, content) in BoardList" :key="content">
-         {{ title }}
-      </li>
-    </div>
-    <span>{{title}}</span>
+    </div> -->
+
+    <li v-for="(listyo, index) in vuelist" :key="index">
+      글 : {{ listyo.title }} 내용 : {{ listyo.content }}
+    </li>
+    <div><button @click="BoardList">조회</button></div>
     <div>
       <button @click="write">글 작성</button>
-      
-      <!-- <div><input type="text" v-model="this.BoardList" /></div> -->
-       <a style="cursor: pointer" @click="this.BoardList">조회</a>
-       <!-- 일단 콘솔로 데이터가 잘 올라오는건 확인  -->
-       
+      <!-- 일단 콘솔로 데이터가 잘 올라오는건 확인  -->
     </div>
   </div>
 </template>
@@ -29,16 +24,19 @@
 // import func from 'vue-editor-bridge';
 import axios from "axios";
 export default {
-    data: () => ({
+  data: () => ({
     //기능, 주소 , 내용
-      title: "",
-      content:"",
+    vuelist: [],
+    // titlecontent: {
+    //   title: '',
+    //   content:'',
+    // }
   }),
   // BoardList 페이지에 들어오면 따로 버튼이 아니라 바로 board가 조회될 수 있도록
   // created 단계에 조회 함수를 실행
   // 일반적으로 조회함수 이름을 search라 하는편, 순수한 검색은 다른이름으로 다시만들것
   created() {
-    // this.search();
+    this.BoardList;
   },
   methods: {
     write: function () {
@@ -49,21 +47,17 @@ export default {
       axios
         .get("http://localhost:8090/board/list")
         .then((result) => {
-          console.log(result)
-          this.title = result.data.title
-          this.content = result.data.content
-          
+          console.log(result);
+          this.vuelist = result.data;
+          // this.content = result.data.content;
         })
         .catch((err) => {
           console.log(err);
-        }); 
+        });
     },
-    search: function () {}, 
+    search: function () {},
   },
-  
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
